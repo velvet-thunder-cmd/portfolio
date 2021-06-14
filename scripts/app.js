8f8b5eb78c9426e8869a8ffed094a8f19b7dbd0b
Sri Ram <69103845+velvet-thunder-cmd@users.noreply.github.com>
2021-06-14 14:15:56 +0530

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+const text = ['Data Science','Machine Learning','Full Stack'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+    if (count === text.length){
+    count=0;
+}
+    currentText=text[count];
+    letter=currentText.slice(0,++index);
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length===currentText.length){
+        count++;
+        index=0;
+    }
+    setTimeout(type,150);
+}());
